46ac976494fa0a4d3c4c989184cc02d0141c3291
simon <zengxianghong@vendoroad.com>
2026-03-24 20:57:57 +0800

    fix:修复bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    // KSP 必须与 Hilt 在同一作用域声明，否则类加载器不同会导致构建失败
+    // 参考：https://github.com/google/dagger/issues/3965
+    alias(libs.plugins.ksp) apply false
 }
