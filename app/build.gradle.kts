db7917e7f566f05f015d98f15d975535ea41c58d
ulpyuxa <ulpyuxa@163.com>
2026-04-09 19:35:29 +0800

    feat:正式版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.simon.trackail"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10101
-        versionName = "1.1.1"
+        versionCode = 10200
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
