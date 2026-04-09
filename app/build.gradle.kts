aa95186ccd91195d52363ef82b1618dcd7563caa
ulpyuxa <ulpyuxa@163.com>
2026-04-09 13:07:46 +0800

    fix:修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.simon.trackail"
         minSdk = 26
         targetSdk = 35
-        versionCode = "1.0.1"
-        versionName = "1.0"
+        versionCode = 10100
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
