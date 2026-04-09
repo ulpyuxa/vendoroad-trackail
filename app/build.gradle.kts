a052e06a10cc9a51ac35761213a87a5cfa8db501
ulpyuxa <ulpyuxa@163.com>
2026-04-09 20:28:20 +0800

    feat: 默认英语语言-正式版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.simon.trackail"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10200
-        versionName = "1.2.0"
+        versionCode = 10101
+        versionName = "1.1.1"
         resourceConfigurations.addAll(listOf("en", "zh"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
